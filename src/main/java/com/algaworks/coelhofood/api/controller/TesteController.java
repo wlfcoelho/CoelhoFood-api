a84e2e8178100efaa95764a747e7e7673bf4ec31
Wilson Coelho <wlfcoelho@gmail.com>
2022-09-27 22:53:43 -0300

    usando o lazy e invertendo a dependência

--- src/main/java/com/algaworks/coelhofood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/coelhofood/api/controller/TesteController.java
@@ -78,7 +78,7 @@ public class TesteController {
 	@GetMapping("/restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome) {
 		
-		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
+		return restauranteRepository.findComfreteGratis(nome);
 	}
 	
 }
--- src/main/java/com/algaworks/coelhofood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/coelhofood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.algaworks.coelhofood.infrastructure.repository;
 
+import static com.algaworks.coelhofood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.coelhofood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,15 +10,15 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.coelhofood.domain.model.Restaurante;
+import com.algaworks.coelhofood.domain.repository.RestauranteRepository;
 import com.algaworks.coelhofood.domain.repository.RestauranteRepositoryQueries;
 
 @Repository
@@ -23,6 +26,9 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired @Lazy //o lazy somente executa a injeção de dependência quando é necessário, caso contrário ficaria redundante
+	private RestauranteRepository restauranteRepository;
 
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -52,5 +58,11 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 
+	}
+
+	@Override
+	public List<Restaurante> findComfreteGratis(String nome) {
+		
+		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
 	}
 }
