46ac7649b6ea39717484def58617152e76c07ae9
Wilson Coelho <wlfcoelho@gmail.com>
2023-09-13 23:48:24 -0300

    criando uma classe que válida uma classe como um todo - no ex temos mais de um ponto que validamos (frete, nome)

---
+++ src/main/java/com/algaworks/coelhofood/core/validation/ValorZeroIncluiDescricao.java
@@ -0,0 +1,28 @@
+package com.algaworks.coelhofood.core.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {ValorZeroIncluiDescricaoValidator.class })
+public @interface ValorZeroIncluiDescricao {
+
+    String message() default "{múltiplo inválido}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    String valorField();
+
+    String descricaoField();
+
+    String descricaoObrigatoria();
+
+}
